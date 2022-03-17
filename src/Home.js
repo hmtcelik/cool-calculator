82c3b17ce190f273469846606273eed9e9b10ded
hmtcelik <abhmtcelik@gmail.com>
2022-03-17 23:28:54 +0300

    calculate logic and finish the project

--- src/Home.js
+++ src/Home.js
@@ -8,18 +8,124 @@ export const ACTIONS = {
     CLEAR: 'clear',
     DELETE_DIGIT: 'delete-digit',
     CHOOSE_OPERATION: 'choose-op',
-    CALCULATE: 'calculate'
+    EVELUATE: 'eveluate'
 }
 
 function reducer(state, {type, payload}){
     switch(type){
         case ACTIONS.ADD_DIGIT:
+            if (state.overwrite){
+                return{
+                    ...state,
+                    currentop: payload.digit,
+                    overwrite: false,
+                }
+            }    
+            if (payload.digit === "0" && state.currentop === "0"){
+                return state
+            } 
+            if (payload.digit === "." && state.currentop.includes(".")) {
+                return state
+            } 
+            
             return{
                 ...state,
                 currentop: `${state.currentop || ""}${payload.digit}`
             }
-    }
+        
+        case ACTIONS.CHOOSE_OPERATION:
+            if (state.currentop == null && state.previousop == null){
+                return state
+            }
+            if (state.currentop == null){
+                return{
+                    ...state,
+                    operation: payload.operation,
+                }
+            }
+            if (state.previousop == null ){
+                return{
+                    ...state,
+                    operation: payload.operation,
+                    previousop: state.currentop,
+                    currentop: null,
+                }
+            }
+            
+            // DO ALWAYS:
+            return{
+                ...state,
+                previousop: eveluate(state),
+                operation: payload.operation,
+                currentop: null,
+            }
+    
+        case ACTIONS.CLEAR:
+            return {}
 
+        case ACTIONS.DELETE_DIGIT:
+            if (state.overwrite) {
+                return{
+                    ...state,
+                    overwrite: false,
+                    currentop: null,
+                }
+            }
+            if (state.currentop == null ) return state
+            if (state.currentop.length === 1){
+                return{ 
+                    ...state, 
+                    currentop:null,
+                }
+            }
+            
+            // DO ALWAYS:
+            return{
+                ...state,
+                currentop: state.currentop.slice(0, -1)
+            }
+
+
+
+        case ACTIONS.EVELUATE:
+            if(state.operation == null || state.currentop == null || state.previousop == null){
+                return state
+            }
+                
+                // DO ALWAYS:
+            return{
+                ...state,
+                previousop: null,
+                operation: null,
+                currentop: eveluate(state)
+            }
+
+        
+    }
+}
+
+
+function eveluate({ currentop, previousop, operation}){
+    const prev = parseFloat(previousop)
+    const current = parseFloat(currentop)
+    if (isNaN(prev) || isNaN(current)) return ""
+    let computation = ""
+    switch (operation){
+        case "+":
+            computation = prev + current
+            break
+        case "-":
+            computation = prev - current
+            break
+        case "*":
+            computation = prev * current
+            break
+        case "/":
+            computation = prev / current
+            break
+    } 
+    
+    return computation.toString()
 }
 
 const Home = () => {
@@ -31,24 +137,24 @@ const Home = () => {
                 <div className="previous-result">{previousop}  {operation}</div>
                 <div className="result">{currentop}</div>
             </div>
-            <button className="span-two">AC</button>
-            <button>DEL</button>
-            <OpButton op="*" editstate={editstate} />
-            <Buttons digit="1" editstate={editstate} />
-            <Buttons digit="2" editstate={editstate} />
-            <Buttons digit="3" editstate={editstate} />
-            <OpButton op="/" editstate={editstate} />
-            <Buttons digit="4" editstate={editstate} />
-            <Buttons digit="5" editstate={editstate} />
-            <Buttons digit="6" editstate={editstate} />
-            <OpButton op="+" editstate={editstate} />
+            <button className="span-two" onClick={() => editstate({ type: ACTIONS.CLEAR })}>AC</button>
+            <button onClick={() => editstate({ type: ACTIONS.DELETE_DIGIT })} >DEL</button>
+            <OpButton operation="/" editstate={editstate} />
             <Buttons digit="7" editstate={editstate} />
             <Buttons digit="8" editstate={editstate} />
             <Buttons digit="9" editstate={editstate} />
-            <OpButton op="-" editstate={editstate} />
+            <OpButton operation="*" editstate={editstate} />
+            <Buttons digit="4" editstate={editstate} />
+            <Buttons digit="5" editstate={editstate} />
+            <Buttons digit="6" editstate={editstate} />
+            <OpButton operation="-" editstate={editstate} />
+            <Buttons digit="1" editstate={editstate} />
+            <Buttons digit="2" editstate={editstate} />
+            <Buttons digit="3" editstate={editstate} />
+            <OpButton operation="+" editstate={editstate} />
             <Buttons digit="." editstate={editstate} />
             <Buttons digit="0" editstate={editstate} />
-            <button className="span-two">=</button>
+            <button className="span-two" onClick={() => editstate({ type: ACTIONS.EVELUATE })} >=</button>
         </div>
     );
 }
--- src/OpButtons.js
+++ src/OpButtons.js
@@ -1,7 +1,7 @@
 import { ACTIONS } from "./Home"
 
-export default function OpButton({ editstate, op }){
-    return <button onClick={() => editstate({ type: ACTIONS.CHOOSE_OPERATION, payload: {op} })}>
-        {op}
+export default function OpButton({ editstate, operation }){
+    return <button onClick={() => editstate({ type: ACTIONS.CHOOSE_OPERATION, payload: {operation} })}>
+        {operation}
     </button>
 }
